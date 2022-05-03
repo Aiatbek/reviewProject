fa8d64f26c8b0959c6f2cef0d48bd2a7052113cb
Aiatbek Kubanov <aiatkubanov@gmail.com>
2022-05-03 22:12:53 +0600

    Problem solved with fn outsorcing and calling event listeners inside the createElement, inside eventListeners fns calling outsorced functions, so that direct recursion will not happen

--- index.js
+++ index.js
@@ -1,48 +1,58 @@
 import { users } from "./data.js"
 const container = document.querySelector('.container')
 // const buttons = document.querySelector('.buttons')
-
 let index = 0
-createElement(index)
-
 function createElement(index){
     container.innerHTML = ''
-    container.innerHTML = `
+    const user  = `
     <div class="img-wrapper">
     <img src="${users[index].img}" alt="">
     </div>
     <h4>${users[index].userName}</h4>
-        <h3>${users[index].position}</h3>
-        <P>${users[index].quote}</P>
-        <div class="buttons">
-            <button class="p"><i class="fas fa-chevron-left fa-2x"></i></button>
-            <button class="n"><i class="fas fa-chevron-right fa-2x"></i></button>
-            </div>
-            <button class='random'>Surprise me</button>
-            `
+    <h3>${users[index].position}</h3>
+    <P>${users[index].quote}</P>
+    <div class="buttons">
+    <button class="p"  id=${index}><i class="fas fa-chevron-left fa-2x"></i></button>
+    <button class="n"><i class="fas fa-chevron-right fa-2x"></i></button>
+    </div>
+    <button class='random' onclick='random()'>Surprise me</button>
+    `
+    container.innerHTML = user
+
+    const prevItem = document.querySelector(".p")
+    prevItem.addEventListener("click",function(){
+        prev()
+    })
+
+    const nextItem = document.querySelector(".n")
+    nextItem.addEventListener("click",function(){
+        next()
+    })
+
+    const randomBtn = document.querySelector('.random')
+    randomBtn.addEventListener('click', function(){
+        random()
+    })
+}
+
+
+    function prev(){
+        if(index > 0){
+            index--
+            createElement(index)
         }
+    }
 
-        const pBtn = document.querySelector('.p')
-        pBtn.addEventListener('click', ()=> {
-            if(index > 0){
-                index--
-                createElement(index) 
-            }
-        })
-        
+    function next(){
+        if(index < users.length - 1){
+            index++
+            createElement(index)
+        }
+    }
 
-        const nBtn = document.querySelector('.n')
-        nBtn.addEventListener('click', ()=> {
-            if(index < users.length - 1){
-                index++
-                createElement(index)
-            }
-        })
+    function random(){
+        let randomIndex = Math.ceil(Math.random()*users.length - 1)
+        createElement(randomIndex)
+    }
 
-        const random = document.querySelector('.random')
-        random.addEventListener('click', ()=>{
-            let randomIndex = Math.ceil(Math.random()*users.length - 1)
-            createElement(randomIndex)
-        })
-
-
+    createElement(index)
